99daed0dcc6c752e8b92b33dbe742d469cff8908
SIMMI <simmi03singh@gmail.com>
2023-11-11 23:33:58 +0530

    updates

---
+++ src/MarketBasketCustomerProductReview/components/data_transformation.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+from dataclasses import dataclass
+from src.MarketBasketCustomerProductReview.exception import customexception
+from src.MarketBasketCustomerProductReview.logger import logging
+
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+
+from src.MarketBasketCustomerProductReview.utils.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+        
+    
+    def get_data_transformation(self):
+        
+        try:
+            logging.info('Data Transformation initiated')
+            
+            # Define which columns should be ordinal-encoded and which should be scaled
+            #categorical_cols = ['cut', 'color','clarity']
+            numerical_cols = ['Avg. Session Length', 'Time on App',	'Time on Website', 'Length of Membership']
+            
+            # Define the custom ranking for each ordinal variable
+            #cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
+            #color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
+            #clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
+            
+            logging.info('Pipeline Initiated')
+            
+            ## Numerical Pipeline
+            num_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='median')),
+                ('scaler',StandardScaler())
+
+                ]
+
+            )
+            
+            
+            preprocessor_pipeline = Pipeline(steps=[
+                
+                ('preprocessor', num_pipeline),
+                ('classifier', RandomForestClassifier())
+            ])
+                       
+            return preprocessor_pipeline
+            
+
+            
+            
+        
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+
+            raise customexception(e,sys)
+            
+    
+    def initialize_data_transformation(self,train_path,test_path):
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+            
+            logging.info("read train and test data complete")
+            logging.info(f'Train Dataframe Head : \n{train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head : \n{test_df.head().to_string()}')
+            
+            preprocessing_obj = self.get_data_transformation()
+            
+            target_column_name = 'price'
+            drop_columns = [target_column_name,'id']
+            
+            input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_column_name]
+            
+            
+            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df=test_df[target_column_name]
+            
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            
+            logging.info("Applying preprocessing object on training and testing datasets.")
+            
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            
+            logging.info("preprocessing pickle file saved")
+            
+            return (
+                train_arr,
+                test_arr
+            )
+            
+        except Exception as e:
+            logging.info("Exception occured in the initiate_datatransformation")
+
+            raise customexception(e,sys)
---
+++ src/MarketBasketCustomerProductReview/pipelines/training_pipeline.py
@@ -0,0 +1,25 @@
+from src.MarketBasketCustomerProductReview.components.data_ingestion import DataIngestion
+
+#from src.MarketBasketCustomerProductReview.components.data_transformation import DataTransformation
+
+#from src.MarketBasketCustomerProductReview.components.model_trainer import ModelTrainer
+
+
+
+import os
+import sys
+from src.MarketBasketCustomerProductReview.logger import logging
+from src.MarketBasketCustomerProductReview.exception import customexception
+import pandas as pd
+
+obj=DataIngestion()
+
+train_data_path,test_data_path=obj.initiate_data_ingestion()
+
+#data_transformation=DataTransformation()
+
+#train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+
+#model_trainer_obj=ModelTrainer()
+#model_trainer_obj.initate_model_training(train_arr,test_arr)
